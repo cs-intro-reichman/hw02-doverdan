f68a8a92d44884d25bef514f25798435fc08b7d7
Dan Dover <45949873+doverdan@users.noreply.github.com>
2024-01-02 00:28:35 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -11,7 +11,7 @@ public class Divisors {
 		/// Printing all the divisors of the given number
 		for (int i = 1; i <= n; i++) {
 			if (n % i == 0) {
-				System.out.print(i + " ");
+				System.out.println(i);
 			}
 		}
 	}
